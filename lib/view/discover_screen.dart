dfdc593bad496f1f201836a10db555101dcd12da
Zonetto <as076053@gmail.com>
2024-08-08 10:19:22 +0300

    UPD, Add Scan Cubit

--- lib/view/discover_screen.dart
+++ lib/view/discover_screen.dart
@@ -3,10 +3,14 @@ import 'package:fire_detection_app/components/my_card_widget.dart';
 import 'package:fire_detection_app/components/tap_Extinguishers.dart';
 import 'package:fire_detection_app/model/extinguisher_model.dart';
 import 'package:fire_detection_app/utils/app_color.dart';
+import 'package:fire_detection_app/utils/my_alert_dialog.dart';
 import 'package:fire_detection_app/utils/responsive_screen.dart';
 import 'package:fire_detection_app/utils/s_of.dart';
 import 'package:fire_detection_app/utils/tab_bar.dart';
+import 'package:fire_detection_app/view_model/scan_cubit/scan_cubit.dart';
+import 'package:fire_detection_app/view_model/scan_cubit/scan_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class DiscoverScreen extends StatefulWidget {
   const DiscoverScreen({super.key});
@@ -45,11 +49,13 @@ class _DiscoverScreenState extends State<DiscoverScreen>
       );
     },
   );
+  ScanCubit _scanCubit = ScanCubit();
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 2, vsync: this);
+    _scanCubit = context.read<ScanCubit>();
   }
 
   @override
@@ -78,15 +84,25 @@ class _DiscoverScreenState extends State<DiscoverScreen>
           tabs: tabs,
         ),
         actions: [
-          IconButton(
-            icon: Icon(
-              Icons.qr_code,
-              color: Colors.black,
-              size: Dimensions.screenHeight(context) * 0.045,
-            ),
-            onPressed: () {
-              //  scan.scanQR(context);
+          BlocListener<ScanCubit, ScanState>(
+            listener: (context, state) {
+              if (state is ScanSuccessfulState) {
+                myAlertDialog(
+                  context: context,
+                  barCodeInfo: state.barcodeScanRes,
+                );
+              }
             },
+            child: IconButton(
+              icon: Icon(
+                Icons.qr_code,
+                color: Colors.black,
+                size: Dimensions.screenHeight(context) * 0.045,
+              ),
+              onPressed: () {
+                _scanCubit.qr(context);
+              },
+            ),
           ),
         ],
       ),
--- lib/view/not_discover_screen.dart
+++ lib/view/not_discover_screen.dart
@@ -6,10 +6,26 @@ import 'package:fire_detection_app/utils/app_color.dart';
 import 'package:fire_detection_app/utils/my_alert_dialog.dart';
 import 'package:fire_detection_app/utils/responsive_screen.dart';
 import 'package:fire_detection_app/utils/s_of.dart';
+import 'package:fire_detection_app/view_model/scan_cubit/scan_cubit.dart';
+import 'package:fire_detection_app/view_model/scan_cubit/scan_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class NotDiscoverScreen extends StatelessWidget {
+class NotDiscoverScreen extends StatefulWidget {
   const NotDiscoverScreen({super.key});
+
+  @override
+  State<NotDiscoverScreen> createState() => _NotDiscoverScreenState();
+}
+
+class _NotDiscoverScreenState extends State<NotDiscoverScreen> {
+  ScanCubit _scanCubit = ScanCubit();
+
+  @override
+  void initState() {
+    super.initState();
+    _scanCubit = context.read<ScanCubit>();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,15 +46,25 @@ class NotDiscoverScreen extends StatelessWidget {
       appBar: appBarWidget(
         title: SOf.text(context).firefighters,
         actions: [
-          IconButton(
-            icon:  Icon(
-              Icons.qr_code,
-              color: Colors.black,
-              size: Dimensions.screenHeight(context) * 0.045,
-            ),
-            onPressed: () {
-              //  scan.scanQR(context);
+          BlocListener<ScanCubit, ScanState>(
+            listener: (context, state) {
+              if (state is ScanSuccessfulState) {
+                myAlertDialog(
+                  context: context,
+                  barCodeInfo: state.barcodeScanRes,
+                );
+              }
             },
+            child: IconButton(
+              icon: Icon(
+                Icons.qr_code,
+                color: Colors.black,
+                size: Dimensions.screenHeight(context) * 0.045,
+              ),
+              onPressed: () {
+                _scanCubit.qr(context);
+              },
+            ),
           ),
         ],
       ),
